b17b5b66644f31c6b1e02cea16a301c68056966c
hukhho <hukhho@gmail.com>
2023-03-09 16:57:19 +0700

    ok

---
+++ src/main/java/com/example/invoicemanagementsystem/InvoiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoicemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoicemanagementsystem/InvoiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
