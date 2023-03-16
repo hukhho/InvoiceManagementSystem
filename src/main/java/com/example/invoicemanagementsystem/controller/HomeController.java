9957d257cdd5603a699a0c6f5f3084c6e7a0b99c
MarkP6464 <quangminhphamtran@gmail.com>
2023-03-16 16:22:04 +0700

    import xml and convert to dto

--- src/main/java/com/example/invoicemanagementsystem/controller/HomeController.java
+++ src/main/java/com/example/invoicemanagementsystem/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.invoicemanagementsystem.controller;
 
+import com.example.invoicemanagementsystem.entity.ExcelTemplate;
 import com.example.invoicemanagementsystem.entity.Seller;
+import com.example.invoicemanagementsystem.service.ExcelFileService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/home")
 public class HomeController {
+    @Autowired
+    ExcelFileService excelFileService;
+
     @RequestMapping("")
     public String home(HttpSession session, Model model) {
         Seller seller = (Seller) session.getAttribute("seller");
         if (seller == null) {
             return "redirect:/auth/login";
         }
+
+        model.addAttribute("excelFiles", excelFileService.getAllExcelFiles());
         model.addAttribute("username", seller.getUsername());
         return "home";
     }
---
+++ src/main/java/com/example/invoicemanagementsystem/controller/XmlController.java
@@ -0,0 +1,46 @@
+package com.example.invoicemanagementsystem.controller;
+
+import com.example.invoicemanagementsystem.dto.Invoice;
+import com.example.invoicemanagementsystem.entity.ExcelTemplate;
+import com.example.invoicemanagementsystem.service.XmlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.xml.transform.stream.StreamSource;
+
+@Controller
+@RequestMapping("/xml")
+public class XmlController {
+
+
+    @PostMapping("/import")
+    public String create(@ModelAttribute("template") String template,
+                         @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", "Please select a file to upload.");
+        } else if (template.isEmpty()){
+            redirectAttributes.addFlashAttribute("error", "Please select a template to upload.");
+        }else {
+            try {
+                Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+                marshaller.setClassesToBeBound(Invoice.class);
+                marshaller.afterPropertiesSet();
+                //invoice object đã đc convert
+                Invoice invoice = (Invoice) marshaller.unmarshal(new StreamSource(file.getInputStream()));
+
+                redirectAttributes.addFlashAttribute("success", "Excel file has been created successfully.");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("error", "Failed to create Excel file.");
+            }
+        }
+        return "redirect:/excel-files";
+    }
+}
